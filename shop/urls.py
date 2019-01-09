5cd1dec40674f9df79fbd8eeeca515d666d7c89b
NOR <inlight.ur.life@gmail.com>
2019-01-09 15:29:37 +0000

    second commit, routing for seller added

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), 
-    path('register', views.register, name='register'),
+    path('product/<int:pk>/', views.product, name='product'),
+    path('product/add_product', views.add_product, name='add_product'),
+    path('product/<int:pk>/edit/', views.edit_product, name='edit_product'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from .models import Products
+from .addform import AddForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html', {})
+    products = Products.objects.all()
+    return render(request, 'shop/index.html', {'products':products})
 
-def register(request):
-    
-    return render(request, 'shop/register.html',{})
+def product(request, pk):
+    product = get_object_or_404(Products, pk=pk)
+    return render(request, 'shop/product.html', {'product':product})
+
+def add_product(request):
+    if request.method == "POST":
+        form = AddForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.auther = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('product', pk=post.pk)
+    else:
+        form = AddForm()
+    return render(request, 'shop/add_product.html', {'form': form})
+
+def edit_product(request, pk):
+    post = get_object_or_404(Products, pk=pk)
+    if request.method == "POST":
+        form = AddForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.auther = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('product', pk=post.pk)
+    else:
+        form = AddForm(instance=post)
+    return render(request, 'shop/add_product.html', {'form': form})
+
+
